6d15b7d3ef17275444ef6a153942cb683e2916e8
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2017-10-04 15:57:38 +0100

    implements req/response classes

--- satori/apps/satori_express.cpp
+++ satori/apps/satori_express.cpp
@@ -19,76 +19,173 @@
 using namespace std;
 using namespace satori;
 
-struct Connection {
+struct HttpRequest {
   std::string url;
   std::string method;
   std::map<std::string, std::string> headers;
   std::vector<std::string> params;
-  Loop* loop;
-  Tcp* client;
 };
 
 
-struct Response {
-
+struct ResponseBuffer {
   Loop* loop;
   Tcp* client;
+  std::string buffer = "";
 
-  std::map<std::string, std::string> headers;
-  int code;
-  std::string buffer;
+  ResponseBuffer(Loop* loop, Tcp* client)
+    : loop{loop}
+    , client{client}
+  {}
+
+  ResponseBuffer& protocol(std::string const& proto = "HTTP/1.0") {
+    return write(proto+" ");
+  }
+
+  ResponseBuffer& status(unsigned short const code) {
+    return write(code2Msg(code));
+  }
+
+  ResponseBuffer& set(std::map<std::string, std::string> const& headers) {
+    for (auto const& e : headers) {
+      write(e.first+": "+ e.second+"\r\n");
+    }
+    return write("\r\n");
+  }
+  
+  ResponseBuffer& write(std::string const& str) {
+    buffer += str; 
+    return *this;
+  }
+
+  Write* flush() {
+    auto w = loop->newWrite(client, buffer);
+    buffer = "";
+    return w;
+  }
+
+  void end(std::string const& str = "") {
+    write(str)
+      .flush()->onWriteEnd = [client = this->client](auto) {
+        client->close();   
+    };
+    client = nullptr;
+  }
+
+  ~ResponseBuffer() {
+    if (client) end();
+  }
+
+};
 
 
-  Response& set(std::map<std::string, std::string> const& headers) {
-    for (auto const& e: headers) {
-      headers[e.first] = e.second;
+struct HttpResponse {
+  std::shared_ptr<ResponseBuffer> buffer;
+
+  std::map<std::string, std::string> headers = {};
+  unsigned short statusCode = 200;
+  bool headersSent = false;
+  std::string protocol = "HTTP/1.0";
+
+  HttpResponse(Loop* loop, Tcp* tcp)
+    : buffer(std::make_shared<ResponseBuffer>(loop, tcp))
+  {}
+
+  HttpResponse(HttpResponse const& r) 
+    : buffer(r.buffer)
+  {}
+
+  HttpResponse(HttpResponse && r) 
+    : buffer(std::move(r.buffer))
+  {}
+
+  HttpResponse() = delete;
+  ~HttpResponse(){}
+
+  HttpResponse http(std::string const& str) {
+    protocol = "HTTP/"+str;
+    return *this;
+  }
+
+  HttpResponse& status(unsigned short const status) {
+    statusCode = status;
+    return *this;
+  }
+
+  HttpResponse& set(std::string const& key, std::string const& value) {
+    headers[key] = value;
+    return *this;
+  }
+
+  HttpResponse& set(std::map<std::string, std::string> const& hdrs) {
+    for (auto const& e : hdrs) {
+      set(e.first, e.second);
     }
     return *this;
   }
 
+  HttpResponse& write(std::string const& str) {
+    assert(buffer && "write after close");
+    if (!headersSent) {
+      buffer
+        ->protocol(protocol)
+        .status(statusCode)
+        .set(headers);
+      headersSent = true;
+    }
 
-  Response& status(unsigned short const& code) {
-    this->code = code;
+    buffer->write(str);
+    return flush();
+  }
+
+  HttpResponse& flush() {
+    buffer->flush();
     return *this;
-
-  }
-  
-  Response& write(std::string const& str) {
-
   }
 
-  Response& end(std::string const& str) {
-
+  HttpResponse& end(std::string const& str="") {
+    buffer->end(str);
+    return *this;
   }
 
+  void close() {
+    buffer.reset();
+  }
 
 };
 
-template<class X>
-void chain(X) {}
 
-template<class X, class F>
-void chain(X const& x, F const& f) {
-  f(x);
+
+
+template<class X, class Y>
+constexpr void chain(X&&, Y&&) {}
+
+template<class X, class Y, class F>
+constexpr void chain(X&& req, Y&& res, F&& f) {
+  f( std::forward<X>(req), std::forward<Y>(res) );
 }
 
-template<class X, class F, class...Fs>
-void chain(X const& x, F const& f, Fs const&...fs) {
-  f(x, [=](auto y) -> decltype(chain(y, fs...)) {
-    chain(y, fs...);
+template<class X, class Y, class F, class...Fs>
+constexpr void chain(X&& req, Y&& res, F&& f, Fs const&...fs) {
+  f(std::forward<X>(req), 
+    std::forward<Y>(res), 
+    [fs...](auto&& req, auto&& res) {
+      chain(
+        std::forward<decltype(req)>(req), 
+        std::forward<decltype(res)>(res), 
+        fs...);
   });
 }
 
-template<class X>
+template<class Req, class Res>
 struct Route {
   std::string route;
   std::string method;
-  std::function<void(X)> handler;
+  std::function<void(Req, Res)> handler;
 
   Route(
       std::string const& route, 
       std::string const& method,  
-      std::function<void(X)> const& handler)
+      std::function<void(Req, Res)> const& handler)
     : route{route}
     , method{method}
     , handler{handler}
@@ -96,42 +193,43 @@ struct Route {
 };
 
 
-template<class X = Connection>
+template<class Req = HttpRequest, class Res = HttpResponse>
 struct AppRouter {
-  std::vector<Route<X>> routes;
+  std::vector<Route<Req, Res>> routes;
   
   template<class F, class...Fs>
   AppRouter& get(std::string const& path, F f, Fs...fs) {
-    routes.emplace_back(Route<X>  {path, "GET", [=](auto x) {
-      chain(x, f, fs...);
+    routes.emplace_back(Route<Req, Res>  {path, "GET", [=](auto x, auto y) {
+      chain(x, y, f, fs...);
     }});
 
     return *this;
   }
 
-
-
   void apply(Loop* loop, Tcp* server) {
 
-    auto router = make_shared<satori::Router<Route<X>>>();
+    auto router = make_shared<satori::Router<Route<Req, Res>>>();
 
-    for (auto r : routes) {
+    for (auto const& r : routes) {
       router->addRoute(r.route, r);
     }
 
     router->compile();
 
 
-    server->onListen = [=](auto status) {
+    server->onListen = [=](int status) {
+      if (status<0) return; // flaky client
+
       auto client = loop->newTcp();
       server->accept(client);
 
       client->read();
 
+      // TODO: make parser part of the connection/request state...
       auto parser = std::make_shared<satori::HttpParser>(HTTP_REQUEST);
       
       client->onData = [parser](const char* str, size_t len) {
-        parser->execute(std::string(str,len));
+        parser->execute(std::string(str, len));
       };
 
     
@@ -144,7 +242,6 @@ struct AppRouter {
         http_parser const* parser, 
         char const* at, 
         size_t len) {
-        //std::cout << "key: " <<*key << std::endl;
         *key = std::string(at, len);
         return 0;
       };
@@ -154,26 +251,31 @@ struct AppRouter {
           char const* at, 
           size_t len) {
 
-        //std::cout << "header: "<<  *key <<"="<<  std::string(at, len) << std::endl;
+        //TODO: avoid pointer indirection
         (*headers)[*key] = std::string(at, len);
         return 0;
       };
 
       parser->onHeadersComplete = [=](auto) {
-        Connection con;
-        con.client = client;
-        con.url = *url;
-        con.headers = *headers;
-        if(auto match = router->match(con.url)) {
-          auto data = match.value.data;
-          auto handler = data.handler;
-          con.loop = loop;
-          con.params = match.value.params;
-          handler(con);
+        if (auto match = router->match(*url)) {
+          auto const& data = match.value.data;
+          auto const& handler = data.handler;
+
+          handler({
+              *url,
+              "GET",
+              *headers,
+              match.value.params
+          }, {
+              loop,
+              client
+          });
+
+
         } else {
-          loop->newWrite(con.client, "HTTP/1.0 404 NOT FOUND\r\n")
+          loop->newWrite(client, "HTTP/1.0 404 NOT FOUND\r\n")
             ->onWriteEnd = [=](auto){
-            con.client->close();
+            client->close();
           };
         }
         return 0;
@@ -188,9 +290,7 @@ struct AppRouter {
         return 0;
       };
     };
-
   }
-  
 
 };
 
@@ -201,41 +301,29 @@ int main() {
 
   AppRouter<> app;
 
-  app.get("/", 
-      //parseHeaders("contentType", "accept"), 
-      [=](auto con) {
-    loop->newWrite(con.client, "HTTP/1.0 200 OK\r\n\r\n");
-      
-    std::string res = "headers:\n";
-    for(auto h: con.headers) {
-      res += h.first + ": " + h.second + "\n";
-    }
-
-    loop->newWrite(con.client, res)
-      ->onWriteEnd = [con](auto) {
-      con.client->close();
-    };
+  app.get("/", [] (auto req, auto res) { 
+    res.status(200)
+      .write("hello world!");
   });
 
 
-  app.get("/{id}", [=](auto con) {
-    loop->newWrite(con.client, "HTTP/1.0 200 OK\r\n\r\n");
-      
-    std::string res = "headers:\n";
-    for(auto h: con.headers) {
-      res += h.first + ": " + h.second + "\n";
+  app.get("/{id}", [](auto req, auto res) {
+    
+    std::string response = "headers:\n";
+    for(auto const& h: req.headers) {
+      response += h.first + ": " + h.second + "\n";
     }
 
-    res += "params: ";
-    for(auto p: con.params) {
-      res += p;
+    response += "params: ";
+    for (auto const& p: req.params) {
+      response += p;
     }
 
-    loop->newWrite(con.client, res)
-      ->onWriteEnd = [con](auto) {
-      con.client->close();
-    };
+    res.status(200)
+      .write(response);
   });
+
+
 
   auto server = loop->newTcp();
   server->listen("127.0.0.1", 8080);
